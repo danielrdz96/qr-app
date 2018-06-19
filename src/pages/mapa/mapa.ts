a1a3078659b90f2eb8a6e2042b98ee9aa30bf550
unknown <daniel_rodriguez_46@hotmail.com>
2018-06-18 21:24:57 -0500

    initial commit

---
+++ src/pages/mapa/mapa.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavParams, ViewController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-mapa',
+  templateUrl: 'mapa.html'
+})
+export class MapaPage {
+
+  lat: number;
+  lng: number;
+
+
+  constructor( public navParams: NavParams,
+               private viewCtrl: ViewController ) {
+
+     this.lat = 24.5592665;
+     this.lng = -104.6587821;
+
+    let coordsArray = this.navParams.get("coords").split(",");
+
+    this.lat = Number( coordsArray[0].replace("geo:","") );
+    this.lng = Number( coordsArray[1] );
+
+
+    console.log(  this.lat, this.lng );
+
+  }
+
+
+  cerrar_modal(){
+    this.viewCtrl.dismiss();
+  }
+
+
+}
